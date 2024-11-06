071b049af99db76b2d4590e3fdc724423b053d35
Chaizee <zuzindima26@gmail.com>
2024-11-06 11:59:16 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Dynamic;
+using System.Transactions;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] mus = new int[n];
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            mus[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            int number = mus[i];
+            int chet = 0;
+            int prom = 0;
+            while (number > 0) {
+                if (( number % 10) % 2 == 0)
+                {
+                    chet++;
+                }
+                number/=10;
+                prom++;
+            }
+            if (chet == prom)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
